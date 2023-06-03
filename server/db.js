655a01920c0e7349c7e0b9107dfa10f1897d055f
CleveyP <clevelandplonsey@gmail.com>
2023-06-03 04:01:34 -0700

    create db connection

---
+++ server/db.js
@@ -0,0 +1,30 @@
+const {MongoClient} = require('mongodb');
+
+async function main(){
+   
+    // const uri = "mongodb+srv://root:battery99@localhost:/?retryWrites=true&w=majority";
+    const uri = "mongodb://localhost:27017";
+
+    const client = new MongoClient(uri);
+ 
+    try {
+        // Connect to the MongoDB cluster
+        await client.connect();
+        
+        //select the cleveland user
+        const database =  client.db("Numerex");
+        const users = database.collection("users");
+        const data = await users.findOne({name: "Pluto"});
+        if(data)
+        console.log(data.name);
+        else{
+            console.log("f")
+        }
+    } catch (e) {
+        console.error(e);
+    } 
+}
+
+main().catch(console.error);
+
+module.exports = database;
